7daaf37022dd5a59f7889c6d6429751935e11754
hoxo-m <hoxo.smile@gmail.com>
2014-11-28 19:28:03 +0900

    [change] change test for a_find()

--- tests/testthat/test-a_find.R
+++ tests/testthat/test-a_find.R
@@ -1,11 +1,11 @@
 context("Test for a_find()")
 
-test_that("find first odd number", {
-  expect_equal(a_find(1:10, x: x %% 2 == 0), 2)
+test_that("find F letter", {
+  expect_equal(a_find(LETTERS, x: x == "F"), "F")
 })
 
 context("Test for Find_()")
 
-test_that("find first odd number", {
-  expect_equal(Find_(1:10, x: x %% 2 == 0), 2)
+test_that("find F letter", {
+  expect_equal(Find_(LETTERS, x: x == "F"), "F")
 })
