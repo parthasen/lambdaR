0f95ac731d5058c99e6f06acb1e5a978c6497020
hoxo-m <hoxo.smile@gmail.com>
2014-11-28 18:43:16 +0900

    add a_mapu()

--- R/a_map.R
+++ R/a_map.R
@@ -3,10 +3,34 @@
 #'@param data a vector.
 #'@param ... lambda expression.
 #'
+#'@return a list.
+#'
 #'@export
 a_map <- function(data, ...) {
   if(!is.list(data)) data <- list(data)
   func <- lambda(...)
   args <- c(f=func, data)
   do.call(Map, args)
+}
+
+#'Map function for lambda expression
+#'
+#'@param data a vector.
+#'@param ... lambda expression.
+#'
+#'@return a list.
+#'
+#'@export
+Map_ <- a_map
+
+#'Map function for lambda expression with unlisted output
+#'
+#'@param data a vector.
+#'@param ... lambda expression.
+#'
+#'@return a vector.
+#'
+#'@export
+a_mapu <- function(data, ...) {
+  unlist(a_map(data, ...))
 }
--- tests/testthat/test-Map_.R
+++
@@ -1,9 +0,0 @@
-context("Test for Map_()")
-
-test_that("compute square number", {
-  expect_equal(Map_(1:5, x: x ** 2), list(1,4,9,16,25))
-})
-
-test_that("add", {
-  expect_equal(Map_(list(1:5, 6:10), x,y: x+y), list(7,9,11,13,15))
-})
