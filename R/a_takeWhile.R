7d3658534c97edec904f3933504f0c500f842d93
hoxo-m <hoxo.smile@gmail.com>
2014-11-28 20:43:35 +0900

    [fix] a_takeWhile()

--- R/a_takeWhile.R
+++ R/a_takeWhile.R
@@ -8,14 +8,14 @@
 #'@examples
 #'library(lambdaR)
 #'library(dplyr)
-#'1:10 %>% a_takeWhile(x: x %% 2 == 0)
+#'1:10 %>% a_takeWhile(x: x < 3)
 #'
 #'@export
 a_takeWhile <- function(data, ..., nomatch = NULL) {
-  index = a_position(data, ...)
-  if(is.na(index)) {
+  index = a_position(data, a_negate(...))
+  if(is.na(index) || index <= 1) {
     return(nomatch)
   } else {
-    return(data[seq_len(index)])
+    return(data[seq_len(index-1)])
   }
 }
--- tests/testthat/test-a_takeWhile.R
+++ tests/testthat/test-a_takeWhile.R
@@ -1,5 +1,13 @@
 context("Test for a_takeWhile()")
 
-test_that("take while odd number", {
-  expect_equal(a_takeWhile(1:10, x: x %% 2 == 0), 1:2)
+test_that("take while x < 3", {
+  expect_equal(a_takeWhile(1:10, x: x < 3), 1:2)
+})
+
+test_that("nomatch", {
+  expect_equal(a_takeWhile(1:10, x: x > 3), NULL)
+})
+
+test_that("input empty vector", {
+  expect_equal(a_takeWhile(c(), x: x < 3), NULL)
 })
